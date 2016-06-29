f43f7dcc1e0786d9217110672d1da3be270f1f31
jpepin <jpepin@jpepin.(none)>
2016-06-29 13:00:17 +0200

    ok

---
+++ Synthe_Final/Display.c
@@ -0,0 +1,131 @@
+/*
+ * File:   Display.c
+ * Author: Aydm
+ *
+ * Created on June 9, 2016, 5:08 AM
+ */
+
+#include "SynthyTwo.h"
+
+void      waitXticks(u32 x)
+{
+    u32 i = 0;
+    while (i < x)
+        i++;
+    return;
+}
+
+void      lcd_pulse(void)
+{
+    _E = 0;
+    waitXticks(1);
+    _E = 1;
+    waitXticks(1);        //enable pulse lasts 100us
+    _E = 0;
+    waitXticks(10);
+    return;
+}
+
+void      write4bits(u8 value)
+{
+    _DB4 = (value & 0x01);
+    _DB5 = ((value >> 1) & 0x01);
+    _DB6 = ((value >> 2) & 0x01);
+    _DB7 = ((value >> 3) & 0x01);
+    lcd_pulse();
+    return;
+}
+
+void      lcd_command(u8 value, u8 rs)
+{
+    static u8 pulseSent = 0;     //protection octet non transmis
+
+    if (pulseSent)
+    {
+        lcd_pulse();
+        pulseSent = 0;
+    }
+
+    if (rs)                  //set RS to write or read
+        _RS = 1;
+    else
+        _RS = 0;
+    write4bits(value >> 4);
+    pulseSent = 1;
+    write4bits(value);
+    pulseSent = 0;
+
+    return;
+}
+
+
+void      lcd_printnbr(u16 value)
+{
+    if (value >= 10)
+        lcd_printnbr(value / 10);
+    lcd_command((value % 10) + '0', 1);
+    return;
+}
+
+
+void      lcd_print(char *str)
+{
+    int x = 0;
+
+    while (str[x])
+    {
+        lcd_command(str[x], 1);
+        x++;
+    }
+    return;
+}
+
+
+void      lcd_goto(u8 col, u8 line)
+{
+    u8 x;
+    if (line == 1)
+        x = 0b10000000;
+    else if (line == 2)
+        x = 0b11000000;
+    else
+        return;
+    if (col >= 1 && col <= 16)
+    {
+        x |= col - 1;
+        lcd_command(x, 0);
+    }
+}
+
+void      init_lcd(void)
+{
+    //-----  PAD IO  ----
+    TRIS_RS = OUTPUT;          //RS is output
+    TRIS_E = OUTPUT;           //E is output
+    TRIS_DB4 = OUTPUT;         //DB4 is output
+    TRIS_DB5 = OUTPUT;         //DB5 is output
+    TRIS_DB6 = OUTPUT;         //DB6 is output
+    TRIS_DB7 = OUTPUT;         //DB7 is output
+
+    _RS = 0;
+    _E = 0;
+    _DB4 = 0;
+    _DB5 = 0;
+    _DB6 = 0;
+    _DB7 = 0;
+
+    waitXticks(3000);         //Wait 3s for LCD to power up to 5V
+    write4bits(0b0010);          //Set 4 bit mode
+    waitXticks(1000);
+    lcd_command(0b00101000, 0);  //Set 4bit mode, 2line display, 5*8 char
+    waitXticks(1000);
+    lcd_command(0b00000110, 0);  //Set DDRam incrementation, Display Shift OFF
+    waitXticks(1000);
+    lcd_command(0b00000001, 0);  //Clear display
+    waitXticks(5000);
+    lcd_command(0b00001100, 0);  //Set Diplay ON, Cursor OFF, Blinking OFF
+    waitXticks(1000);
+
+    return;
+}
+
---
+++ Synthe_Final/SynthyTwo.h
@@ -0,0 +1,289 @@
+/* 
+ * File:   SynthyTwo.h
+ * Author: Aydm
+ *
+ * Created on June 9, 2016, 5:02 AM
+ */
+
+#ifndef SYNTHYTWO_H
+#define	SYNTHYTWO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p32xxxx.h>
+#include <sys/attribs.h>
+
+typedef unsigned    char u8;
+typedef unsigned    short u16;
+typedef unsigned    long u32;
+typedef signed      char s8;
+typedef signed      short s16;
+typedef signed      long s32;
+
+# define TRUE 1
+# define FALSE 0
+# define INPUT 1
+# define OUTPUT 0
+# define VCC 1
+# define GND 0
+
+# define MONTANT    0
+# define DESCENDANT 1
+
+# define ATK        0
+# define DEC        1
+# define SUS        2
+# define REL        3
+
+# define SQU        0
+# define TRI        1
+# define SAW        2
+# define SIN        3
+
+#define MIDI_CHANNEL 1
+
+
+//-----------  SWITCH  -------------
+#define TRIS_SW1    TRISFbits.TRISF1
+#define TRIS_SW2    TRISEbits.TRISE0
+#define TRIS_SW3    TRISEbits.TRISE5
+#define TRIS_SW4    TRISBbits.TRISB1
+#define TRIS_SW5    TRISBbits.TRISB0
+#define _SW1        PORTFbits.RF1
+#define _SW2        PORTEbits.RE0
+#define _SW3        PORTEbits.RE5
+#define _SW4        PORTBbits.RB1
+#define _SW5        PORTBbits.RB0
+
+//------------  LEDS  -------------
+#define TRIS_LED0   TRISDbits.TRISD5
+#define TRIS_LED1   TRISDbits.TRISD6
+#define TRIS_LED2   TRISDbits.TRISD7
+#define TRIS_LED3   TRISEbits.TRISE6
+#define TRIS_LED4   TRISFbits.TRISF0
+#define TRIS_LED5   TRISGbits.TRISG6
+#define TRIS_LED6   TRISEbits.TRISE7
+#define TRIS_LED7   TRISGbits.TRISG8
+#define TRIS_LED8   TRISGbits.TRISG7
+#define _LED0       LATDbits.LATD5
+#define _LED1       LATDbits.LATD6
+#define _LED2       LATDbits.LATD7
+#define _LED3       LATEbits.LATE6
+#define _LED4       LATFbits.LATF0
+#define _LED5       LATGbits.LATG6
+#define _LED6       LATEbits.LATE7
+#define _LED7       LATGbits.LATG8
+#define _LED8       LATGbits.LATG7
+
+//---------  Encodeurs  -------------
+#define TRIS_EAA    TRISEbits.TRISE1
+#define TRIS_EAB    TRISEbits.TRISE2
+#define TRIS_EBA    TRISEbits.TRISE3
+#define TRIS_EBB    TRISEbits.TRISE4
+#define TRIS_ECA    TRISBbits.TRISB5
+#define TRIS_ECB    TRISBbits.TRISB4
+#define TRIS_EDA    TRISBbits.TRISB3
+#define TRIS_EDB    TRISBbits.TRISB2
+#define _EAA        PORTEbits.RE1
+#define _EAB        PORTEbits.RE2
+#define _EBA        PORTEbits.RE3
+#define _EBB        PORTEbits.RE4
+#define _ECA        PORTBbits.RB5
+#define _ECB        PORTBbits.RB4
+#define _EDA        PORTBbits.RB5
+#define _EDB        PORTBbits.RB6
+
+//----------  MULTIPLEX  -----------
+#define TRIS_MULT0  TRISCbits.TRISC14
+#define TRIS_MULT1  TRISCbits.TRISC13
+#define _MULT0      LATCbits.LATC14
+#define _MULT1      LATCbits.LATC13
+
+//-----------  Display  -------------
+# define TRIS_E TRISBbits.TRISB10
+# define TRIS_RS TRISBbits.TRISB11
+# define TRIS_DB4 TRISBbits.TRISB12
+# define TRIS_DB5 TRISBbits.TRISB13
+# define TRIS_DB6 TRISBbits.TRISB15
+# define TRIS_DB7 TRISFbits.TRISF4
+
+# define _E LATBbits.LATB10
+# define _RS LATBbits.LATB11
+# define _DB4 LATBbits.LATB12
+# define _DB5 LATBbits.LATB13
+# define _DB6 LATBbits.LATB15
+# define _DB7 LATFbits.LATF4
+
+
+typedef     struct  s_env
+{
+    u8          Atk;
+    u8          Dec;
+    u8          Sus;
+    u8          Rel;
+
+    u16         compteur;
+    u8          cur_env;
+    u16         cur_amplitude;
+    u16         amplitude_max;
+    u16         seuil_max;
+    u16         seuil_min;
+    u16         env_reste;
+}                   t_env;
+
+typedef     struct  s_osc
+{
+    u16         jumper;
+    u8          jump_adjust;
+    u16         pos;
+
+    u16         pitch;
+    u8          pitch_adjust;
+
+    u16         phi;
+    u16         phi_adjust;
+
+    u16         cursor;
+    u8          phase;
+    u8          reste;
+
+    t_env       Env;
+    u8          presetOnde;
+
+}                   t_osc;
+
+extern u16         tab_car[1420];
+extern u16         tab_saw[1420];
+extern u16         tab_tri[1420];
+extern u16         tab_sin[1420];
+
+extern u8          notesON;
+extern u8          midi_note;
+extern u16         DAC_value;
+
+extern t_osc       OSC1;
+extern t_osc       OSC2;
+
+//------------------  Encodeurs  ----------------------
+extern u8          prev_status;
+extern u8          status;
+# define _RD4      PORTDbits.RD4       // PIN A
+# define _RD5      PORTDbits.RD5       // PIN B
+
+
+extern u8          prev_status2;
+extern u8          status2;
+extern u8          boutons;
+# define SW1       0x02
+# define SW2       0x04
+# define SW3       0x08
+# define SW4       0x10
+# define SW5       0x20
+//------------------  Encodeurs  ----------------------
+
+
+//------------------------- AudioBuffer.c -----------------------
+//||                                                           ||
+extern u8      onde;    // Forme de l'onde (change à l'appui du bouton)
+extern u16     cursor;   // Donne la cadence d'augmentation de la tension
+extern u16     pitch;
+extern u8      pitch_adjust;
+extern u8      phase;
+extern u16     phi;
+extern u8      reste;
+
+u16     generateAudioSample(void);
+//void __ISR(_TIMER_2_VECTOR, IPL1AUTO) sampleClock(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//---------------------------- DAC.c ----------------------------
+//||                                                           ||
+extern u16     DAC_value;       // VALUE EST LA TENSION (1 à FFFF) QUI VA AU DAC !
+
+void         init_dac(void);
+//void __ISR(_SPI_2_VECTOR, IPL3AUTO) sendSampleToDAC(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//-------------------------- Display.c --------------------------
+//||                                                           ||
+void      init_lcd(void);
+void      waitXticks(u32 x);
+void      lcd_pulse(void);
+void      write4bits(u8 value);
+void      lcd_command(u8 value, u8 rs);
+void      lcd_printnbr(u16 value);
+void      lcd_print(char *str);
+void      lcd_goto(u8 col, u8 line);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//-------------------------- Enveloppe.c --------------------------
+//||                                                           ||
+extern u8      cur_env;
+extern u16     compteur;
+extern u16     cur_amplitude;
+
+void            generateur_enveloppe(t_env *Env);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//--------------------------- MIDI.c ----------------------------
+//||                                                           ||
+extern u8      notesON;                  // nombre de notes enfoncees
+extern u8      midi_note;                // note a jouer
+
+void      init_MIDI(void);
+void      processMIDIBuffer(void);
+//void __ISR(_UART_1_VECTOR, IPL7SOFT) recieveMIDIbyte(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//------------------------ Oscillator.c -------------------------
+//||                                                           ||
+//static void         onde_carre(void);
+//static void         onde_triangle(void);
+//static void         onde_sawtooth(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+//-------------------------- Preset.c ---------------------------
+//||                                                           ||
+void    init_preset(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+//------------------------ TabCreate.c -------------------------
+//||                                                           ||
+extern u32      tab_period[88];
+
+void    create_tab_period(void);
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+//--------------------------- Main.c ----------------------------
+//||                                                           ||
+extern u32     tab_period[88];         // Tableau contenant toutes les périodes
+//void    tab_create(u32 *tab_period);
+//void  main();
+//||                                                           ||
+//---------------------------------------------------------------
+
+
+
+#endif	/* SYNTHYTWO_H */
+
